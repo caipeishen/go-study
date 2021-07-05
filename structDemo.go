d70f8550807117f29176d94944c7131c442f0a81
Cai Peishen <peishen.cai@foxmail.com>
2021-07-05 15:42:25 +0800

    结构

---
+++ structDemo.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"fmt"
+	"encoding/json"
+)
+
+type Cat struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+
+func main(){
+	var myCat Cat
+	myCat.Id = 10
+	myCat.Name = "喵咪"
+	fmt.Println(myCat)
+	myFun1(myCat)
+	fmt.Println("值传递：", myCat)
+	myFun2(&myCat)
+	fmt.Println("引用传递：", myCat)
+	jsonByte, err :=  json.Marshal(myCat)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(jsonByte))
+}
+// 值传递
+func myFun1(myCat Cat) {
+	myCat.Name = "我改变了喵咪名字，喵呜~"
+}
+// 引用传递
+func myFun2(myCat *Cat) {
+	(*myCat).Name = "我改变了喵咪名字，喵呜~"
+}
