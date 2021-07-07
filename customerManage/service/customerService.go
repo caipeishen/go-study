475619fd47eadce909e2208c5708490389420438
Cai Peishen <peishen.cai@foxmail.com>
2021-07-07 16:40:30 +0800

    客户管理系统基本完成

---
+++ customerManage/service/customerService.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"fmt"
+	model "go-study/customerManage/model"
+)
+
+type CustomerService struct { 
+	Customers []model.Customer //声明一个字段，表示当前切片含有多少个客户
+}
+
+// 客户service工厂
+func NewCustomerService() *CustomerService {
+	customerService := &CustomerService{}
+	customer := model.NewCustomer(1, "Ferris", "男", 21, "110", "peishen.cai@foxmail.com")
+	customerService.Customers = append(customerService.Customers, customer)
+	return customerService
+}
+
+// 添加客户
+func (cs *CustomerService) AddCustomer(name string, gender string, age int, phone string, email string) {
+	cs.Customers = append(cs.Customers, model.NewCustomer(len(cs.Customers)+1, name, gender, age, phone, email))
+	return
+}
+
+// 返回列表数据
+func (cs *CustomerService) List() []model.Customer {
+	return cs.Customers
+}
+
+// 根据id获取客户
+func (cs *CustomerService) GetById(id int) int {
+	index := -1
+	for i, v := range cs.Customers {
+		if id == v.GetId() {
+			index = i
+			break
+		}
+	}
+	return index
+}
+
+// 根据id删除客户
+func (cs *CustomerService) DeleteId(id int) {
+	index := cs.GetById(id)
+	if index == -1 {
+		fmt.Println("改用户不存在,无法删除")
+	} else {
+		cs.Customers = append(cs.Customers[:index], cs.Customers[index+1:]...)
+
+	}
+}
--- customerManage/view/main.go
+++ customerManage/view/main.go
@@ -7,12 +7,10 @@ import(
 
 
 type customerView struct {
-	// 定义必要字段
 	key string // 接收用户输入...
 	loop bool	// 表示是否循环的显示主菜单
 	customerService *service.CustomerService // 增删改查service
 }
-
 
 // 显示主菜单
 func (this *customerView) mainMenu() {
@@ -29,13 +27,13 @@ func (this *customerView) mainMenu() {
 
 		switch this.key {
 			case "1":
-				fmt.Println("添加客户")
+				this.Add()
 			case "2":	
-				fmt.Println("修改客户")
+				this.Reset()
 			case "3":
-				fmt.Println("删除客户")
+				this.DeleteCustomer()
 			case "4":
-				fmt.Println("客户列表")
+				this.List()
 			case "5":
 				this.loop = false
 			default :
@@ -48,16 +46,102 @@ func (this *customerView) mainMenu() {
 	}
 	fmt.Println("你退出了客户关系管理系统...")
 }
-func main() {
-	// 在main 函数中，创建一个 customerView,并运行显示主菜单.. 
-	customerView := customerView {
-		key : "", 
-		loop : true,
+
+// 添加客户
+func (c *customerView) Add() {
+	var name string
+	fmt.Println("请输入用户姓名")
+	fmt.Scanln(&name)
+	var gender string
+	fmt.Println("请输入性别")
+	fmt.Scanln(&gender)
+	var age int
+	fmt.Println("请输入年龄")
+	fmt.Scanln(&age)
+	var phone string
+	fmt.Println("请输入电话号码")
+	fmt.Scanln(&phone)
+	var email string
+	fmt.Println("请输入邮箱")
+	fmt.Scanln(&email)
+	c.customerService.AddCustomer(name, gender, age, phone, email)
+}
+
+// 修改客户
+func (c *customerView) Reset() {
+	var id int
+	fmt.Println("请输入需要修改的用户ID(无需请输入-1)")
+	fmt.Scanln(&id)
+	if id == -1 {
+		return
+	}
+	resetIndex := c.customerService.GetById(id)
+	if resetIndex == -1 {
+		fmt.Println("改用户不存在")
+	} else {
+		_, name, gender, age, phone, email := c.customerService.List()[resetIndex].GetInfo()
+		fmt.Printf("姓名(%v):", name)
+		fmt.Scanln(&name)
+		fmt.Printf("性别(%v):", gender)
+		fmt.Scanln(&gender)
+		fmt.Printf("年龄(%v):", age)
+		fmt.Scanln(&age)
+		fmt.Printf("电话(%v):", phone)
+		fmt.Scanln(&phone)
+		fmt.Printf("邮箱(%v):", email)
+		fmt.Scanln(&email)
+		c.customerService.List()[resetIndex].Reset(name, gender, age, phone, email)
 	}
 
-	//这里完成对 customerView 结构体的 customerService 字段的初始化
-	customerView.customerService = service.NewCustomerService()
+}
 
-	// 显示主菜单.. 
-	customerView.mainMenu()
+// 删除客户
+func (c *customerView) DeleteCustomer() {
+	var id int
+	fmt.Println("请输入需要删除的用户ID(无需请输入-1)")
+	fmt.Scanln(&id)
+	var choice string
+	if id == -1 {
+		return
+	} else {
+		fmt.Printf("你确定删除ID为%v的客户吗?请输入y/n\n", id)
+	}
+	fmt.Scanln(&choice)
+	for {
+		if choice == "y" || choice == "Y" {
+			c.customerService.DeleteId(id)
+			fmt.Println("-------------------------客户删除成功-------------------------")
+			break
+		} else if choice == "n" || choice == "N" {
+			break
+		}
+	}
+}
+
+// 客户列表
+func (c *customerView) List() {
+	var customer = c.customerService.List()
+	fmt.Println("---------------------------客户列表---------------------------")
+	fmt.Println("编号\t姓名\t性别\t年龄\t电话\t邮箱")
+	for _, v := range customer {
+		a, b, c, d, e, f := v.GetInfo()
+		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\n", a, b, c, d, e, f)
+	}
+	fmt.Println("-------------------------客户列表完成-------------------------")
+}
+
+// 初始化客户试图结构体
+func NewCV() *customerView {
+	return &customerView{
+		key:             "",
+		loop:            true,
+		customerService: service.NewCustomerService(), // 完成对 customerView 结构体的 customerService 字段的初始化
+	}
+
+}
+
+// 主方法
+func main() {
+	var cv = NewCV()
+	cv.mainMenu()
 }
