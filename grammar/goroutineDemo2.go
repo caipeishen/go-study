88bb9e1612c9c7ffb083467a1ff22788e24cdb20
Cai Peishen <peishen.cai@foxmail.com>
2021-08-11 10:58:23 +0800

    goroutine和channel协同工作的案例

--- grammar/goroutineDemo2.go
+++ grammar/goroutineDemo2.go
@@ -6,12 +6,12 @@ import (
 
 /**
 请完成goroutine和channel协同工作的案例,具体要求:
-1)开启一个writeData协程,向管道intChan中写入50个整数.
+1)开启一个writeData协程,向管道intChan中写入10个整数.
 2)开启一个readData协程，从管道intChan中读取writeData写入的数据。3)注意:writeData和readDate操作的是同一个管道
 4)主线程需要等待writeData和readDate协程都完成工作才能退出【管道】
 */
 func main() {
-	intChan := make(chan int, 50)
+	intChan := make(chan int, 10)
 	go writeData(intChan)
 
 	exitChan := make(chan bool, 1)
@@ -27,7 +27,8 @@ func main() {
 
 // 写入数据
 func writeData(intChan chan int) {
-	for i := 1; i <= 50; i++ {
+	// 即使写入和读取频率不一样，也没问题，只是在写入时，可能会阻塞
+	for i := 1; i <= 20; i++ {
 		//放入数据
 		intChan <- i
 		fmt.Println("writeData ", i)
