5af0e66e67e5558bca3b87c013bb4ff1d8b2e3de
Cai Peishen <peishen.cai@foxmail.com>
2022-07-29 12:27:42 +0800

    channel实践操作补充

--- go_code/chapter16/channelapply/main.go
+++ go_code/chapter16/channelapply/main.go
@@ -9,16 +9,18 @@ import (
 func writeData(intChan chan int) {
 	for i := 1; i <= 50; i++ {
 		//放入数据
+		//time.Sleep(time.Second * 5)
 		intChan <- i //
 		fmt.Println("writeData ", i)
 		//time.Sleep(time.Second)
 	}
+
+	fmt.Println("关闭iniChan")
 	close(intChan) //关闭
 }
 
 //read data
 func readData(intChan chan int, exitChan chan bool) {
-
 	for {
 		v, ok := <-intChan
 		if !ok {
@@ -33,7 +35,38 @@ func readData(intChan chan int, exitChan chan bool) {
 
 }
 
+func simple() {
+	//创建两个管道
+	intChan := make(chan int)
+	//exitChan := make(chan bool, 1)
+
+	intChan <- 1
+	go func() {
+		<-intChan
+	}()
+	fmt.Printf("intChan:")
+}
+
+func closeDetail() {
+	//创建两个管道
+	intChan := make(chan int, 1)
+	//exitChan := make(chan bool, 1)
+
+	intChan <- 1
+
+	close(intChan)
+
+	num, ok := <-intChan
+	fmt.Printf("num=%v, ok=%v\n", num, ok)
+
+	num2, ok2 := <-intChan
+	fmt.Printf("num2=%v, ok2=%v\n", num2, ok2)
+}
+
 func main() {
+	//simple()
+
+	//closeDetail()
 
 	//创建两个管道
 	intChan := make(chan int, 10)
